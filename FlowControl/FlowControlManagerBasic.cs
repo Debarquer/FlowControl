a4ec02ac4265c81102e3f2fc5555ed8948638c96
Simon Ektjärn <debarquer@gmail.com>
2024-09-12 15:10:43 +0200

    Added input repetitions

--- FlowControl/FlowControlManagerBasic.cs
+++ FlowControl/FlowControlManagerBasic.cs
@@ -9,14 +9,16 @@ namespace FlowControl
     internal class FlowControlManagerBasic : IFlowControlManager
     {
         CinemaTicketManager cinemaTicketManager = new();
+        IterationManager iterationManager = new() { };
 
         public void ManageInput()
         {
             Console.WriteLine("Welcome to the main menu.");
             Console.WriteLine("Navigate the meny by typing the corresponding numbers and pressing enter.");
             Console.WriteLine("Options:");
-            Console.WriteLine("0: Quit the program");
-            Console.WriteLine("1: Retrieve ticket price");
+            Console.WriteLine("0: Quit the program.");
+            Console.WriteLine("1: Retrieve ticket price.");
+            Console.WriteLine("2: Repeat a message ten times.");
 
             do
             {
@@ -29,6 +31,9 @@ namespace FlowControl
                         break;
                     case "1":
                         cinemaTicketManager.PrintTicketPrice();
+                        break;
+                    case "2":
+                        iterationManager.RepeatTenTimes();
                         break;
                     default:
                         Console.WriteLine("Invalid input.");
---
+++ FlowControl/IterationManager.cs
@@ -0,0 +1,25 @@
+﻿using MenuHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl
+{
+    internal class IterationManager(int repetitions = 10)
+    {
+        int Repetitions { get; set; } = repetitions;
+
+        public void RepeatTenTimes()
+        {
+            string input = Utilities.PromptUserForString("Please enter a string: ");
+
+            for(int i = 0; i < Repetitions; i++)
+            {
+                Console.Write($"{i + 1}. {input}" + (i == Repetitions-1 ? "" : ", "));
+            }
+            Console.WriteLine();
+        }
+    }
+}
