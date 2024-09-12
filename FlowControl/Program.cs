01c2fe0a596ecaa5228b7dfc372de2a8ff23872d
Simon Ektjärn <debarquer@gmail.com>
2024-09-12 14:47:37 +0200

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,10 @@
+﻿using FlowControl;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IFlowControlManager controlManager = new FlowControlManagerBasic();
+        controlManager.ManageInput();
+    }
+}
