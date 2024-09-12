2a0d57c5e768fd2761a403b91462b0c0becbfede
Simon Ektjärn <debarquer@gmail.com>
2024-09-12 14:58:59 +0200

    Added calculate single ticket price

---
+++ FlowControl/CinemaTicketManager.cs
@@ -0,0 +1,53 @@
+﻿using MenuHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl
+{
+    internal class CinemaTicketManager
+    {
+        Menu menu = new();
+
+        const int youthPrice = 80;
+        const int pensionersPrice = 90;
+        const int defaultPrice = 120;
+
+        public void PrintTicketPrice()
+        {
+            int ticketPrice = CalculateTicketPrice();
+
+            Console.WriteLine($"Ticket price: {ticketPrice}");
+        }
+
+        private int CalculateTicketPrice()
+        {
+            int ticketPrice = 0;
+
+            int age = PromptUserForAge();
+
+            switch(age)
+            {
+                case < 20:
+                    ticketPrice = youthPrice;
+                    break;
+                case > 64:
+                    ticketPrice = pensionersPrice;
+                    break;
+                default:
+                    ticketPrice = defaultPrice;
+                    break;
+            }
+
+            return ticketPrice;
+        }
+
+        private int PromptUserForAge()
+        {
+            return Utilities.PromptUserForValidNumber("Please enter your age: ");
+        }
+    }
+}
--- FlowControl/FlowControlManagerBasic.cs
+++ FlowControl/FlowControlManagerBasic.cs
@@ -8,8 +8,33 @@ namespace FlowControl
 {
     internal class FlowControlManagerBasic : IFlowControlManager
     {
+        CinemaTicketManager cinemaTicketManager = new();
+
         public void ManageInput()
         {
+            Console.WriteLine("Welcome to the main menu.");
+            Console.WriteLine("Navigate the meny by typing the corresponding numbers and pressing enter.");
+            Console.WriteLine("Options:");
+            Console.WriteLine("0: Quit the program");
+            Console.WriteLine("1: Retrieve ticket price");
+
+            do
+            {
+                Console.Write("Input: ");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    case "1":
+                        cinemaTicketManager.PrintTicketPrice();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+            } while (true);
         }
     }
 }
